2cf4f4660bb52b0d8814d700518e1c31b030a042
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-06-16 20:03:35 +0600

    redux setup with api

---
+++ src/components/Posts/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@mui/styles";
+import { createTheme } from "@mui/material";
+const theme = createTheme();
+
+export default makeStyles(() => ({
+    mainContainer: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    smMargin: {
+        margin: theme.spacing(1),
+    },
+    actionDiv: {
+        textAlign: 'center',
+    },
+}));
